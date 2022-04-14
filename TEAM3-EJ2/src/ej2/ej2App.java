5e444173b2ebf8b395ff7b474b030f9f5128c7f8
Ixabel Justo <71872946+Kay-Nicte@users.noreply.github.com>
2022-04-14 12:53:13 +0200

    "Modificar objetos de los arrays"

--- TEAM3-EJ2/src/ej2/ej2App.java
+++ TEAM3-EJ2/src/ej2/ej2App.java
@@ -10,10 +10,10 @@ public class ej2App {
     Serie arraySeries[]=new Serie[5];
 
     //Creamos objetos en cada posicion de arrayVideojuegos
-    arrayVideojuegos[0]=new Videojuego("Tomb Raider: Legend", 7.5, true, "Aventura", "Humansoft");
+    arrayVideojuegos[0]=new Videojuego("Tomb Raider: Legend", 8, "Aventura", "Humansoft");
 
     //Creamos un objeto en cada posición de arraySeries
-    arraySeries[0]=new Serie("Pose", 1, true, "drama", "FX Productions");
+    arraySeries[0]=new Serie("Pose", 1, "drama", "FX Productions");
 	
 	}
 }
