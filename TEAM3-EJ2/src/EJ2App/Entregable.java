70f09b2089f7c518caed5c424d84488aa6950bf0
Ixabel Justo <71872946+Kay-Nicte@users.noreply.github.com>
2022-04-14 11:00:20 +0200

    "Implementar métodos"

--- TEAM3-EJ2/src/EJ2App/Entregable.java
+++ TEAM3-EJ2/src/EJ2App/Entregable.java
@@ -1,7 +1,26 @@
 package EJ2App;
 
 public interface Entregable {
+	
+//Método entregar. Cambia el atributo prestado a true. 
+public static void entregar() {
+	
+}
 
+//Método devolver. Cambia el atributo prestado a false.
+public static void devolver() {
+	
+}
+
+//Devuelve el estado del atributo prestado.
+public static void isEntregado() {
+	
+}
+
+//Compara las horas estimadas en los videojuegos y en las series el número de temporadas
+public static void compareTo(Object a) {
+	
+}
 
 
 }
